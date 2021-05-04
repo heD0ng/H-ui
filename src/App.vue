dddd749665cee2e9b935c850598252ad6543ce10
heD0ng <13545403924@163.com>
2021-05-04 21:31:37 +0800

    button-ui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hm-button>
+      按钮
+    </hm-button>
+    <hm-button type='primary'>
+      按钮
+    </hm-button>
+    <hm-button type='success'>
+      按钮
+    </hm-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    .hm-button{
+      margin-right: 10px;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HmButton from './components/button.vue'
+
+Vue.component(HmButton.name, HmButton)
 
 Vue.config.productionTip = false
 
